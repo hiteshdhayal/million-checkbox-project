e36dbffc7052275e29331d5825ddac470282ae40
hitesh dhayal <hiteshdhayal@gmail.com>
2026-05-04 00:35:07 +0530

    updating redis code

--- backend/src/redis.js
+++ backend/src/redis.js
@@ -3,8 +3,17 @@ require('dotenv').config();
 
 const redisUrl = process.env.REDIS_URL;
 
-const commandClient = new Redis(redisUrl);
-const pubsubClient = new Redis(redisUrl);
+const commandClient = new Redis(redisUrl, {
+  connectTimeout: 5000,
+  commandTimeout: 5000,
+  maxRetriesPerRequest: 1,
+});
+
+const pubsubClient = new Redis(redisUrl, {
+  connectTimeout: 5000,
+  commandTimeout: 5000,
+  maxRetriesPerRequest: 1,
+});
 
 commandClient.on('error', (err) => console.error('Redis Command Client Error', err));
 pubsubClient.on('error', (err) => console.error('Redis PubSub Client Error', err));
